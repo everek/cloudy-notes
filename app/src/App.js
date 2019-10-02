92471e22a80dbdcecc5c4efa0fa3f0dbd418c2c8
Leo Rotzler <leo.rotzler@bkb.ch>
2019-10-02 22:58:38 +0200

    [Update]: Added packages; Added auth

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import ApolloClient from 'apollo-client'
+import { split } from 'apollo-link'
+import { HttpLink } from 'apollo-link-http'
+import { WebSocketLink } from 'apollo-link-ws'
+import { getMainDefinition } from 'apollo-utilities'
+import gql from 'graphql-tag'
+import React from 'react'
+import { ApolloProvider, Mutation, Subscription } from 'react-apollo'
 
-function App() {
+function App({ authState }) {
+  const isIn = authState.status === 'in'
+
+  const headers = isIn ? { Authorization: `Bearer ${authState.token}` } : {}
+
+  const httpLink = new HttpLink({
+    uri: 'https://cloudynotes.herokuapp.com/v1/graphql',
+    headers,
+  })
+
+  const wsLink = new WebSocketLink({
+    uri: 'wss://cloudynotes.herokuapp.com/v1/graphql',
+    options: {
+      reconnect: true,
+      connectionParams: {
+        headers,
+      },
+    },
+  })
+
+  const link = split(
+    ({ query }) => {
+      const { kind, operation } = getMainDefinition(query)
+      return kind === 'OperationDefinition' && operation === 'subscription'
+    },
+    wsLink,
+    httpLink,
+  )
+
+  const client = new ApolloClient({
+    link,
+    cache: new InMemoryCache(),
+  })
+
+  const NOTE = gql`
+  subscription NOTE($userId: String!) {
+    note {
+      title
+    }
+  }
+  `
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <Subscription
+        subscription={NOTE}
+        variables={
+          isIn
+            ? {
+              userId: authState.user.uid,
+            }
+            : null
+        }
+      >
+        {({ data, loading, error }) => {
+          console.log(authState);
+          
+          if (loading) return 'loading...'
+          if (error) return error.message
+          return (
+            <div>
+              {/* @TODO */}
+            </div>
+          )
+        }}
+      </Subscription>
+    </ApolloProvider>
   );
 }
 
